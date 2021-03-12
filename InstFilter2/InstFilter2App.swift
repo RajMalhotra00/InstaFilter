1ebdd1dd4dfaaa230fc1e6ee0fdcb7e93e4bad5e
RajMalHotRA123 <RM_frankfurt@gmx.de>
2021-03-12 10:09:09 +0100

    Initial Commit

---
+++ InstFilter2/InstFilter2App.swift
@@ -0,0 +1,17 @@
+//
+//  InstFilter2App.swift
+//  InstFilter2
+//
+//  Created by Raj Malhotra on 12.03.21.
+//
+
+import SwiftUI
+
+@main
+struct InstFilter2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
