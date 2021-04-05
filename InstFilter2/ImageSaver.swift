c5008e9e9287e0d82f12da8c6c955b5ea489ff35
RajMalHotRA123 <RM_frankfurt@gmx.de>
2021-04-05 15:25:47 +0200

    Added new features

---
+++ InstFilter2/ImageSaver.swift
@@ -0,0 +1,24 @@
+//
+//  ImageSaver.swift
+//  InstFilter2
+//
+//  Created by Raj Malhotra on 16.03.21.
+//
+
+import UIKit
+
+class ImageSaver: NSObject {
+    var successHandler: (() -> Void)?
+    var errorHandler: ((Error) -> Void)?
+    func writeToPhotoAlbum(image: UIImage) {
+        UIImageWriteToSavedPhotosAlbum(image, self, #selector(saveError), nil)
+    }
+    
+    @objc func saveError(_ image: UIImage, didFinishSavingWithError error: Error?, contextInfo: UnsafeRawPointer) {
+        if let error = error {
+            errorHandler?(error)
+        } else{
+            successHandler?()
+        }
+       }
+}
